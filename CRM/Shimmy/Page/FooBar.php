63d3758284b96631dc65c0c2b74c778840eea8be
Tim Otten <totten@civicrm.org>
2021-03-22 00:31:34 -0700

    Add example page

---
+++ CRM/Shimmy/Page/FooBar.php
@@ -0,0 +1,16 @@
+<?php
+use CRM_Shimmy_ExtensionUtil as E;
+
+class CRM_Shimmy_Page_FooBar extends CRM_Core_Page {
+
+  public function run() {
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    CRM_Utils_System::setTitle(E::ts('FooBar'));
+
+    // Example: Assign a variable for use in a template
+    $this->assign('currentTime', date('Y-m-d H:i:s'));
+
+    parent::run();
+  }
+
+}
