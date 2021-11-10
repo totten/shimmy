1694aee9bb1411f076ce63350497c319be9bbbf5
Tim Otten <totten@civicrm.org>
2021-11-09 23:39:30 -0800

    Extract mixin/menu-xml

--- mixin/civix-register-files@2.0.0.mixin.php
+++ mixin/civix-register-files@2.0.0.mixin.php
@@ -2,29 +2,11 @@
 
 /**
  * @param CRM_Extension_MixInfo $mixInfo
- *   On newer deployments, this will be an instance of MixInfo. On older deployments, Civix may polyfill with a work-a-like class.
+ *   On newer deployments, this will be an instance of MixInfo. On older deployments, Civix may polyfill with a work-a-like.
  * @param \CRM_Extension_BootCache $bootCache
- *   On newer deployments, this will be an instance of MixInfo. On older deployments, Civix may polyfill and omit this parameter.
+ *   On newer deployments, this will be an instance of MixInfo. On older deployments, Civix may polyfill with a work-a-like.
  */
 return function ($mixInfo, $bootCache) {
-
-  /**
-   * Auto-register "xml/Menu/*.xml" files.
-   *
-   * @param \Civi\Core\Event\GenericHookEvent $e
-   * @see CRM_Utils_Hook::xmlMenu()
-   */
-  Civi::dispatcher()->addListener('hook_civicrm_xmlMenu', function ($e) use ($mixInfo) {
-    // When deactivating on a polyfill/pre-mixin system, listeners may not cleanup automatically.
-    if (!$mixInfo->isActive()) {
-      return;
-    }
-
-    $files = (array) glob($mixInfo->getPath('xml/Menu/*.xml'));
-    foreach ($files as $file) {
-      $e->files[] = $file;
-    }
-  });
 
   /**
    * Auto-register "xml/case/*.xml" files.
