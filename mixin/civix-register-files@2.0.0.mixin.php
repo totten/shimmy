4c75c400d7b0427dcd16a9b0b5f7e809898f2ebb
Tim Otten <totten@civicrm.org>
2021-11-10 00:32:17 -0800

    Extract mixin/case-xml, mixin/mgd-php, mixin/settings-php

--- mixin/civix-register-files@2.0.0.mixin.php
+++ mixin/civix-register-files@2.0.0.mixin.php
@@ -9,32 +9,6 @@
 return function ($mixInfo, $bootCache) {
 
   /**
-   * Auto-register "xml/case/*.xml" files.
-   *
-   * @param \Civi\Core\Event\GenericHookEvent $e
-   * @see CRM_Utils_Hook::caseTypes()
-   */
-  Civi::dispatcher()->addListener('hook_civicrm_caseTypes', function ($e) use ($mixInfo) {
-    // When deactivating on a polyfill/pre-mixin system, listeners may not cleanup automatically.
-    if (!$mixInfo->isActive() || !is_dir(__DIR__ . '/xml/case')) {
-      return;
-    }
-
-    foreach ((array) glob($mixInfo->getPath('xml/case/*.xml')) as $file) {
-      $name = preg_replace('/\.xml$/', '', basename($file));
-      if ($name != CRM_Case_XMLProcessor::mungeCaseType($name)) {
-        $errorMessage = sprintf("Case-type file name is malformed (%s vs %s)", $name, CRM_Case_XMLProcessor::mungeCaseType($name));
-        throw new CRM_Core_Exception($errorMessage);
-      }
-      $e->caseTypes[$name] = [
-        'module' => $mixInfo->longName,
-        'name' => $name,
-        'file' => $file,
-      ];
-    }
-  });
-
-  /**
    * Auto-register "ang/*.ang.php" files.
    *
    * @param \Civi\Core\Event\GenericHookEvent $e
@@ -78,53 +52,6 @@ return function ($mixInfo, $bootCache) {
         $themeMeta['ext'] = $mixInfo->longName;
       }
       $e->themes[$themeMeta['name']] = $themeMeta;
-    }
-  });
-
-  /**
-   * Auto-register "settings/*.settings.php" files.
-   *
-   * @param \Civi\Core\Event\GenericHookEvent $e
-   * @see CRM_Utils_Hook::alterSettingsFolders()
-   */
-  Civi::dispatcher()->addListener('hook_civicrm_alterSettingsFolders', function ($e) use ($mixInfo) {
-    // When deactivating on a polyfill/pre-mixin system, listeners may not cleanup automatically.
-    if (!$mixInfo->isActive()) {
-      return;
-    }
-
-    $settingsDir = $mixInfo->getPath('settings');
-    if (!in_array($settingsDir, $e->settingsFolders) && is_dir($settingsDir)) {
-      $e->settingsFolders[] = $settingsDir;
-    }
-  });
-
-  /**
-   * Auto-register "**.mgd.php" files.
-   *
-   * @param \Civi\Core\Event\GenericHookEvent $e
-   * @see CRM_Utils_Hook::managed()
-   */
-
-  Civi::dispatcher()->addListener('hook_civicrm_managed', function ($event) use ($mixInfo) {
-    // When deactivating on a polyfill/pre-mixin system, listeners may not cleanup automatically.
-    if (!$mixInfo->isActive()) {
-      return;
-    }
-
-    $mgdFiles = CRM_Utils_File::findFiles($mixInfo->getPath(), '*.mgd.php');
-    sort($mgdFiles);
-    foreach ($mgdFiles as $file) {
-      $es = include $file;
-      foreach ($es as $e) {
-        if (empty($e['module'])) {
-          $e['module'] = $mixInfo->longName;
-        }
-        if (empty($e['params']['version'])) {
-          $e['params']['version'] = '3';
-        }
-        $event->entities[] = $e;
-      }
     }
   });
 
